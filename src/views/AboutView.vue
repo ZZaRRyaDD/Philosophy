4105c2cebc1d146b2b9a206fd9e6dd2be03c97be
Nazar <odddellarobea@gmail.com>
2022-05-26 01:45:32 +0700

    Add answer 'Не знаю', add links, change description project

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -3,7 +3,7 @@
         <div class="px-2">
             <h4 id="scrollspyHeading2">О проекте</h4>
             <p>
-                Проект разработан с целью определения сходимости уровня пользователя с
+                Проект разработан с целью определить, насколько темперамент пользователя схож с 
                 идеями Буддизма.
             </p>
             <h4 id="scrollspyHeading1">Разработчики</h4>
@@ -13,14 +13,14 @@
             <ul class="list-group w-50">
                 <li 
                     class="list-group-item"
-                    v-for="technologue in this.technologues"
-                    :key="this.technologues.indexOf(technologue)"
+                    v-for="tech in this.technology"
+                    :key="this.technology.indexOf(tech)"
                 >
                     <a
                         class="text-dark text-decoration-none"
-                        :href="technologue.url"
+                        :href="tech.url"
                     >
-                        {{ technologue.name }}
+                        {{ tech.name }}
                     </a>
                 </li>
             </ul>
@@ -50,8 +50,13 @@ export default {
                 "https://www.nur.kz/family/self-realization/1379262-buddizm-osnovnyye-idei-i-printsipy/",
                 "https://hbr-russia.ru/liderstvo/delo-zhizni/a13155/",
                 "https://arzamas.academy/mag/599-buddha",
+                "https://studybuddhism.com/ru/prodvinutyy-uroven/istoriya-i-kultura/buddizm-v-sovremennom-mire/buddizm-i-nauka",
+                "https://studybuddhism.com/ru/tibetskiy-buddizm/o-buddizme/mir-buddizma/buddizm-v-sovremennom-mire",
+                "https://studybuddhism.com/ru/prodvinutyy-uroven/istoriya-i-kultura/buddizm-v-sovremennom-mire/privlekatelnost-buddizma-dlya-sovremennogo-mira",
+                "https://buddhayana.ru/%D0%BF%D1%83%D1%82%D1%8C-%D0%B1%D1%83%D0%B4%D0%B4%D1%8B-%D0%B2-%D0%BC%D0%B8%D1%80%D0%B5.html",
+                "https://www.krugosvet.ru/enc/kultura_i_obrazovanie/religiya/BUDDA_I_BUDDIZM.html",
             ],
-            technologues: [
+            technology: [
                 {
                     "url": "https://vuejs.org/",
                     "name": "Vue 3",
